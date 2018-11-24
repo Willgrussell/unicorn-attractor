3097e67097a8d91fd9a9c55783e4cf9ba019198a
William Russell <russellgwill@gmail.com>
2018-11-24 16:52:59 +0000

    changes to tests.py

--- tickets/tests.py
+++ tickets/tests.py
@@ -12,11 +12,4 @@ class TicketTests(TestCase):
     def test_str(self):
         test_issue_name = Ticket(issue_name='issue with site')
         self.assertEqual(str(test_issue_name), 'issue with site')
-        
-# Forms test
-        
-    def test_invalid_form(self):
-        t = Ticket.objects.create(issue_type="Bug", issue_name="Test", issue_detail="large", status="Doing", urgent=False)
-        data = {'issue_type': t.issue_detail, 'issue_name': t.issue_name, 'issue_detail': t.issue_detail, 'status': t.status, 'urgent': False}
-        form = TicketForm(data=data)
-        self.assertFalse(form.is_valid())
+
